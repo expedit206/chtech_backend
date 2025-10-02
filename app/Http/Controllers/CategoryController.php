243d8998a64b67b42c190b0d35acda2b9821d7c3
Expedit206 <mciagnessi@gmail.com>
2025-10-02 13:34:27 +0100

    jj4

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -13,7 +13,7 @@ class CategoryController extends Controller
 {
     public function index()
     { 
-        $categories = Category::orderBy('nom', 'asc');
+        $categories = Category::orderBy('nom', 'asc')->get();
         return response()->json(['categories' => $categories]);
     }
 
