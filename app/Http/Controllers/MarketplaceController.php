570a940cb54dc4e8dc5de5b530a63fb4521129ff
Expedit206 <mciagnessi@gmail.com>
2026-05-04 20:23:50 +0100

    review

--- app/Http/Controllers/MarketplaceController.php
+++ app/Http/Controllers/MarketplaceController.php
@@ -15,7 +15,7 @@ class MarketplaceController extends Controller
     public function getProduits(Request $request): JsonResponse
     {
         try {
-            $user = aut h()->user();
+            $user = auth()->user();
             $query = Produit::with(['category', 'user'])
                 ->where('est_actif', true)
                 ->where('quantite', '>', 0);
